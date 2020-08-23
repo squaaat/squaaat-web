3a6b39069a2ea73351f3742e532e9fd8158aeddf
지녕군 <wls5563@gmail.com>
2020-08-23 13:38:34 +0900

    add eslint, prettier, editerconfig (#8)

--- components/common/Header/Header.tsx
+++ components/common/Header/Header.tsx
@@ -1,24 +1,18 @@
-import React from 'react';
-import './Header.scss';
+import React from "react";
+import "./Header.scss";
 
-
-interface HeaderProps{
-  [key: string]: any;
+interface HeaderProps {
+  [key: string]: string;
 }
-const Header:React.FC<HeaderProps> = () => {
-
+const Header: React.FC<HeaderProps> = () => {
   return (
     <header className="Header">
       <div className="container">
-        <div className="box">
-          Arrow
-        </div>
+        <div className="box">Arrow</div>
         <h1 className="logo-text">!Squaaat</h1>
-        <div className="box">
-          Arrow
-        </div>
+        <div className="box">Arrow</div>
       </div>
     </header>
-  )
-}
+  );
+};
 export default Header;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,83 +1,15 @@
 import React from "react";
 import Layout from "../components/Layout";
 import "styles/base.scss";
-import Amplify, { Auth, Hub } from "aws-amplify";
+import Amplify from "aws-amplify";
 import awsconfig from "../aws-exports.js";
-
-import { AmplifySignOut } from "@aws-amplify/ui-react";
-import Header from "components/common/Header";
 import { NextPage } from "next";
 
 Amplify.configure(awsconfig);
-
 const IndexPage: NextPage = () => {
-  console.log(awsconfig);
-  const [user, setUser] = React.useState<null | string>(null);
-
-  React.useEffect(() => {
-    Hub.listen("auth", ({ payload: { event, data } }) => {
-      console.log(event);
-      switch (event) {
-        case "signIn":
-          console.log(data);
-          setUser(data);
-          break;
-        case "signOut":
-          setUser(null);
-          break;
-      }
-    });
-    Auth.currentAuthenticatedUser()
-      .then(res => {
-        setUser(res);
-        console.log("login...");
-      })
-      .catch(e => {
-        console.log(e);
-        console.log("Not signed in");
-      });
-  }, []);
-
-  function oauth(provider: string) {
-    Auth.federatedSignIn({ customProvider: provider }).then(res => {
-      console.log(res);
-    });
-  }
   return (
     <Layout title={`!Squaaat (${process.env.STAGE})`}>
-      <Header />
-
-      <main style={{ marginTop: 72 }}>
-        <div style={{ background: "red" }}>bbb</div>
-      </main>
-      <br />
-      <br />
-
-      <br />
-
-      {true && (
-        <>
-          {user === null && (
-            <p style={{ textAlign: "center" }}>
-              <button onClick={() => oauth("Google")}>Open Google</button>
-              <button disabled onClick={() => Auth.federatedSignIn({ customProvider: "Facebook" })}>
-                Open Facebook
-              </button>
-              <AmplifySignOut />
-            </p>
-          )}
-          {user && (
-            <div style={{ maxWidth: "768px", margin: "auto" }}>
-              <p>{user.username}</p>
-              <AmplifySignOut />
-            </div>
-          )}
-        </>
-      )}
-      <p>{process.env.AWS_USER_POOLS_ID}</p>
-      <p>{process.env.AWS_COGNITO_IDENTITY_POOL_ID}</p>
-      <p>{process.env.AWS_USER_POOLS_WEB_CLIENT_ID}</p>
-      <p>{JSON.stringify(process.env.OAUTH)}</p>
+      <h1>I do not know </h1>
     </Layout>
   );
 };
