7d82c81c421641db1c7446a610cf8859145a45a2
jinongun <wls5563@gmail.com>
2020-08-23 15:29:42 +0900

    test

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,8 @@
 import React from "react";
 import Layout from "../components/Layout";
 import "styles/base.scss";
-import Amplify from "aws-amplify";
-import awsconfig from "../aws-exports.js";
 import { NextPage } from "next";
 
-Amplify.configure(awsconfig);
 const IndexPage: NextPage = () => {
   return (
     <Layout title={`!Squaaat (${process.env.STAGE})`}>
