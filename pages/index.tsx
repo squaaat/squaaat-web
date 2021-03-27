c44a5a51fa209b146df4374d070c63f9484f7e2c
jinongun <wls5563@gmail.com>
2021-03-27 12:43:46 +0900

    init

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,15 @@
-import React from "react";
-import Layout from "../components/Layout";
-import "styles/base.scss";
-import { NextPage } from "next";
+import Link from 'next/link'
+import Layout from '../components/Layout'
 
-const IndexPage: NextPage = () => {
-  return (
-    <Layout title={`!Squaaat (${process.env.STAGE})`}>
-      <h1>I do not know </h1>
-    </Layout>
-  );
-};
+const IndexPage = () => (
+  <Layout title="Home | Next.js + TypeScript Example">
+    <h1>Hello Next.js 👋</h1>
+    <p>
+      <Link href="/about">
+        <a>About</a>
+      </Link>
+    </p>
+  </Layout>
+)
 
-export default IndexPage;
+export default IndexPage
